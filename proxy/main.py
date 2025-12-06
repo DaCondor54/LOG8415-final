db4b6d85d3e8784539ad565ce43d08031034ddda
DaCondor54 <enrique.llere@gmail.com>
2025-12-06 11:57:56 -0500

    Setup local proxy test

--- proxy/main.py
+++ proxy/main.py
@@ -5,34 +5,54 @@ from pythonping import ping
 from enum import Enum
 from dataclasses import dataclass
 
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, HTTPException
 
 @dataclass
-class Server:
+class ServerCredentials:
     name: str
-    ip: str
     password: str
+
+@dataclass
+class SQLRequest:
+    sql_query: str
+    strategy: str
 
 class ForwardingStrategies(Enum):
     DIRECT = 'direct'
     RANDOM = 'random'
     CUSTOM = 'custom'
 
+class InvalidQueryType(Exception):
+    pass
+
 class Router:
-    source = Server('source', '1.1.1.1', 'P@ssword123')
-    replicas = [Server('replica_1', '1.1.1.1', 'Replic@1'), Server('replica_2', '1.1.1.1', 'Replic@2')]
+    source = '54.87.26.74'
+    replicas = ['54.167.80.3','54.226.0.76']
 
     read_queries = {'SELECT'}
-    write_queries = {'DELETE', 'INSERT'}
+    write_queries = {'DELETE', 'INSERT', 'UPDATE'}
 
-    def __init__(self, forwarding_strategy: ForwardingStrategies):
-        self.forwarding_strategy = forwarding_strategy
+    def __init__(self):
+        self._forwarding_strategy = ForwardingStrategies.CUSTOM
 
-    def route_query(self, sql_query: str) -> Server:
-        return self.select_replica() if self.is_read_query(sql_query) else self.source    
+    def route_query(self, sql_query: str) -> str:
+        if self.is_read_query(sql_query):
+            return self.select_replica()
+        elif self.is_write_query(sql_query):
+            return self.source
+        else:
+            queries = self.read_queries | self.write_queries
+            raise InvalidQueryType(f'is not a {queries}')             
+
+
+    def is_sql_query_of_type(self, sql_query: str, keywords: set[str]) -> bool:
+        return any(sql_query.upper().startswith(word) for word in keywords)
 
     def is_read_query(self, sql_query: str) -> bool:
-        return any(sql_query.upper().startswith(word) for word in self.read_queries)
+        return self.is_sql_query_of_type(sql_query, self.read_queries)
+    
+    def is_write_query(self, sql_query: str) -> bool:
+        return self.is_sql_query_of_type(sql_query, self.write_queries)
 
     def select_replica(self) -> str:
         match self.forwarding_strategy:
@@ -43,8 +63,8 @@ class Router:
             case ForwardingStrategies.CUSTOM:
                 return self.custom_strategy()
             
-    def ping_replica(self, replica: Server):
-        return ping(target = replica.ip, count = 1).rtt_avg_ms
+    def ping_replica(self, replica_ip: str) -> float:
+        return ping(target = replica_ip, count = 1).rtt_avg_ms
 
     def direct_strategy(self) -> str:
         return self.source
@@ -54,26 +74,40 @@ class Router:
 
     def custom_strategy(self) -> str:    
         return min(self.replicas, key=self.ping_replica)
+    
+    @property
+    def forwarding_strategy(self) -> ForwardingStrategies:
+        return self._forwarding_strategy
+    
+    @forwarding_strategy.setter 
+    def forwarding_strategy(self, strategy: str):
+        if strategy in ForwardingStrategies:
+            self._forwarding_strategy = ForwardingStrategies(strategy)
+        else:
+            self._forwarding_strategy = ForwardingStrategies.CUSTOM
 
-forwarding_strategy = ForwardingStrategies(sys.argv[1]) if sys.argv[1] in ForwardingStrategies else ForwardingStrategies.CUSTOM
-router = Router(forwarding_strategy)
+
+router = Router()
+server = ServerCredentials('proxy', 'P@ssword123')
 
 app = FastAPI()
 
 @app.post("/")
-async def receive_sql_query(sql_query: str = Body(...)):
+async def receive_sql_query(request: SQLRequest):
+    try:
+        router.forwarding_strategy = request.strategy
+        server_ip = router.route_query(request.sql_query)
+        return send_sql_query(request.sql_query, server_ip)
+    except InvalidQueryType as e:
+        print(e)
+        raise HTTPException(status_code = 400, detail=str(e))
 
-    server = router.route_query(sql_query)
-    return send_sql_query(sql_query, server)
 
-def send_sql_query(sql_query: str, server: Server):
-    if sql_query != None:
-        return server
-    
+def send_sql_query(sql_query: str, server_ip: str):    
     cnx = None    
     try:
         cnx = pymysql.connect(
-            host=server.ip,
+            host=server_ip,
             user=server.name,
             password=server.password,
             database="sakila"
@@ -84,8 +118,7 @@ def send_sql_query(sql_query: str, server: Server):
         cursor.execute(sql_query)
 
         rows = cursor.fetchall()
-        for row in rows:
-            print(row)
+        return { 'rows': rows, 'ip': server_ip, 'is_source': server_ip == router.source, 'strategy': router.forwarding_strategy }
 
     except pymysql.Error as e:
         print(f"Error connecting to MySQL with PyMySQL: {e}")
--- terraform/scripts/source-server.sh
+++ terraform/scripts/source-server.sh
@@ -11,20 +11,28 @@ sudo sed -i "s/^# server-id.*$/server-id = 100/" "$CONFIG_FILE"
 sudo sed -i "s/^# log_bin/log_bin/" "$CONFIG_FILE"
 sudo sed -i "s/^# binlog_do_db.*$/binlog_do_db = sakila/" "$CONFIG_FILE"
 
-sudo systemctl restart mysql
+sudo systemctl restart mysql 
+
+PROXY="'proxy'@'%'"
+REPLICA_1="'replica_1'@'10.0.4.%'"
+REPLICA_2="'replica_2'@'10.0.4.%'"
 
 sudo mysql <<EOF
 
-CREATE USER 'replica_1'@'10.0.4.%' IDENTIFIED WITH mysql_native_password BY 'Replic@1';
-GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'replica_1'@'10.0.4.%';
+CREATE DATABASE sakila;
 
-CREATE USER 'replica_2'@'10.0.4.%' IDENTIFIED WITH mysql_native_password BY 'Replic@2';
-GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'replica_2'@'10.0.4.%';
+CREATE USER $REPLICA_1 IDENTIFIED WITH mysql_native_password BY 'Replic@1';
+GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO $REPLICA_1;
+
+CREATE USER $REPLICA_2 IDENTIFIED WITH mysql_native_password BY 'Replic@2';
+GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO $REPLICA_2;
+
+CREATE USER $PROXY IDENTIFIED WITH mysql_native_password BY 'P@ssword123';
+GRANT SELECT, UPDATE, INSERT, DELETE ON sakila.* TO $PROXY;
 
 FLUSH PRIVILEGES;
 
 FLUSH TABLES WITH READ LOCK;
 
 UNLOCK TABLES;
-CREATE DATABASE sakila;
 EOF
